084aca9c84f4fefe13adef700ec1a5d43f147e16
nuggsatriaa <nugrahaasatriaaa@gmail.com>
2023-01-02 07:13:02 +0700

    revisian

--- index.js
+++ index.js
@@ -3,3 +3,31 @@ const nav = document.querySelector("nav ul");
 menuToggle.addEventListener("click", function () {
   nav.classList.toggle("slide");
 });
+
+const alertPlaceholder = document.getElementById("liveAlertPlaceholder");
+
+const alert = (message, type) => {
+  const wrapper = document.createElement("div");
+  wrapper.innerHTML = [`<div class="alert alert-${type} alert-dismissible" role="alert">`, `   <div>${message}</div>`, '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>', "</div>"].join("");
+
+  alertPlaceholder.append(wrapper);
+};
+
+const alertTrigger = document.getElementById("liveAlertBtn");
+if (alertTrigger) {
+  alertTrigger.addEventListener("click", () => {
+    alert("Pemesanan telah berhasil, anda akan dihubungi admin kami lewat WA", "success");
+  });
+}
+
+function handleClose() {
+  if (document.getElementById("lezrent")) {
+    document.getElementById("lezrent").classList.remove("show");
+  }
+}
+
+function handleOpen() {
+  if (document.getElementById("lezrent")) {
+    document.getElementById("lezrent").classList.add("show");
+  }
+}
